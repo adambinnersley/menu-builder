b2e8295fa83999e8f1a1340a929b02bb7f94dcfd
Adam Binnersley <abinnersley@gmail.com>
2018-03-27 09:49:09 +0100

    Uncomment active items code

--- src/Builder/Menu.php
+++ src/Builder/Menu.php
@@ -16,7 +16,7 @@ class Menu {
     protected static function createMenuLevel($array, $level, $elements = [], $currentItems = [], $activeClass = 'active', $startLevel = 0, $numLevels = 2){
         $menu = ($level >= $startLevel ? '<ul'.Link::htmlClass($elements['ul_class'], '').Link::htmlID($elements['ul_id']).'>' : '');
         foreach($array as $item){
-            //$active = ($currentItems[$level]['uri'] === $item['uri'] ? trim($activeClass) : '');
+            $active = ($currentItems[$level]['uri'] === $item['uri'] ? trim($activeClass) : '');
             $menu.= ($level >= $startLevel || $currentItems[$level]['uri'] === $item['uri'] ? ($level < $startLevel && $currentItems[$level]['uri'] === $item['uri'] ? (is_array($item['children']) ? self::createMenuLevel($item['children'], ($level + 1), $elements, $currentItems, $activeClass, $startLevel, $numLevels) : '') : (((isset($item['active']) && boolval($item['active']) === true) || !isset($item['active'])) ? '<li'.Link::htmlClass($item['li_class'], $active).Link::htmlID($item['li_id']).'>'.Link::formatLink($item, $active).(is_array($item['children']) && $level < ($startLevel + $numLevels) ? self::createMenuLevel($item['children'], ($level + 1), $item, $currentItems, $activeClass, $startLevel, $numLevels) : '').'</li>' : '')) : '');
         }
         $menu.= ($level >= $startLevel ? '</ul>' : '');
