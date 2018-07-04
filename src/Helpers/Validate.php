cb891baea3723ac1fd0c6b90dbc2631db4e85117
Adam Binnersley <abinnersley@gmail.com>
2018-07-04 11:36:10 +0100

    Make functions static

--- src/Helpers/Validate.php
+++ src/Helpers/Validate.php
@@ -8,7 +8,7 @@ class Validate {
      * @param string $uri This should be the URI you are checking
      * @return boolean If the URI is valid will return true else returns false
      */
-    public function validateURI($uri) {
+    public static function validateURI($uri) {
         if(!empty(trim($uri))){
             return filter_var(trim($uri), FILTER_VALIDATE_URL);
         }
@@ -20,7 +20,7 @@ class Validate {
      * @param string $uri This should be the URI you are sanitising
      * @return string Returns the sanitised URI
      */
-    public function sanitizeURI($uri) {
+    public static function sanitizeURI($uri) {
         return strtolower(filter_var(trim($uri), FILTER_SANITIZE_URL));
     }
 }
