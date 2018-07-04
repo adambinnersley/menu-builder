c9363bf204b17278f8cb27174ff9648d856f59bc
Adam Binnersley <abinnersley@gmail.com>
2018-07-04 11:31:58 +0100

    Add validator and sanitiser for URI

--- src/Helpers/Validate.php
+++ src/Helpers/Validate.php
@@ -3,4 +3,24 @@ namespace Menu\Helpers;
 
 class Validate {
     
+    /**
+     * Validates that a URI is valid
+     * @param string $uri This should be the URI you are checking
+     * @return boolean If the URI is valid will return true else returns false
+     */
+    public function validateURI($uri) {
+        if(!empty(trim($uri))){
+            return filter_var(trim($uri), FILTER_VALIDATE_URL);
+        }
+        return false;
+    }
+    
+    /**
+     * Sanitise a URI 
+     * @param string $uri This should be the URI you are sanitising
+     * @return string Returns the sanitised URI
+     */
+    public function sanitizeURI($uri) {
+        return strtolower(filter_var(trim($uri), FILTER_SANITIZE_URL));
+    }
 }
